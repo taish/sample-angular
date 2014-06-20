291c5c246d5c9985b25a4bc86369d91f931fbe63
taish <kbystis@gmail.com>
2014-06-20 21:50:59 +0800

    add angular-local-storage.js

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -28,6 +28,7 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
+      'bower_components/angular-local-storage/angular-local-storage.js',
       'app/scripts/*.js',
       'app/scripts/**/*.js',
       'test/mock/**/*.js',
